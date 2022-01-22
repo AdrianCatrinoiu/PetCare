c5b5ed588373c16a8ad75b36fa6137381d40ca78
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2022-01-23 01:47:00 +0200

    Make feed sensor to be both water and food

--- BE-Flask/app.py
+++ BE-Flask/app.py
@@ -1,14 +1,8 @@
 from distutils.log import debug
 from flask import Flask
-import sys, os
-
-#aici vom importa fisierele la care lucram pentru a face diferitele elemente
-sys.path.insert(0,'/ButtonWater/ButtonWaterModel')
-
-#aici vom importa fisierele
+import os
 from objects import PetCareObject
 
-#aici vom declara obiectele
 petCareObject = PetCareObject()
 
 app = Flask(__name__)
@@ -23,12 +17,12 @@ def hello_world():
 
 @app.route('/start-water-sensor')
 def startWaterSensor():
-	petCareObject.startSensor()
+	petCareObject.startWaterSensor()
 	return 'Water sensor is opened'
 
 @app.route('/stop-water-sensor')
 def stopWaterSensor():
-	petCareObject.stopSensor()
+	petCareObject.stopWaterSensor()
 	return 'Water sensor is closed'
 
 @app.route('/get-water-level')
@@ -40,6 +34,30 @@ def getWaterLevel():
 def makeWaterEmpty():
 	petCareObject.makeWaterEmpty()
 	return 'make water empty'
+
+"""
+	Buton pentru hranire
+"""
+
+@app.route('/start-food-sensor')
+def startFoodSensor():
+	petCareObject.startFoodSensor()
+	return 'Food sensor is opened'
+
+@app.route('/stop-food-sensor')
+def stopFoodSensor():
+	petCareObject.stopFoodSensor()
+	return 'Food sensor is closed'
+
+@app.route('/get-food-level')
+def getFoodLevel():
+	foodLevel = petCareObject.getFoodLevel()
+	return f'Your food level is {foodLevel}.'
+
+@app.route('/make-food-empty')
+def makeFoodEmpty():
+	petCareObject.makeFoodEmpty()
+	return 'make food empty'
 
 if __name__ == '__main__':
 	os.environ['FLASK_ENV'] = 'development'
