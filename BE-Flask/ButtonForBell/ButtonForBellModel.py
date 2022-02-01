32bced4944343e54e0efb5f7c15aa9fe221ed402
marianeacsu <58784210+marianeacsu@users.noreply.github.com>
2022-02-01 22:05:36 +0200

    Update ButtonForBellModel.py

--- BE-Flask/ButtonForBell/ButtonForBellModel.py
+++ BE-Flask/ButtonForBell/ButtonForBellModel.py
@@ -1,15 +1,23 @@
 import time
 import threading
+import os
+import sys
+from playsound import playsound
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 class ButtonForBell:
-    def __init__(self ,bellTimer):
+    def __init__(self ,bellTimer,bellType):
         self.__bellTimer = bellTimer
         self.__isActive = False
+        self.__bellType = bellType
 
      
      
     def __singing(self):
        self.__isActive = True
+       if self.__bellType == 'Food' or self.__bellType == 'Water':
+           playsound(os.path.join(__location__, 'sound.wav'))
        time.sleep(self.__bellTimer)
        self.__isActive = False
 
