87fec17340c9de837f780e9685efe21ce67579e3
AdrianCatrinoiu <ady.catrinoiu@gmail.com>
2022-02-03 01:26:50 +0200

    update mqtt

--- BE-Flask/app.py
+++ BE-Flask/app.py
@@ -7,6 +7,7 @@ import os
 from threading import Thread
 import time
 import json
+from paho.mqtt import client as mqtt_client
 
 import db
 import environment
@@ -23,7 +24,7 @@ socketio = None
 
 ### swagger specific ###
 SWAGGER_URL = '/swagger'
-API_URL = '/swagger.json'
+API_URL = '/static/swagger.json'
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
@@ -31,39 +32,40 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
         'app_name': "PetCare"
     }
 )
+app = None
+client = None
+broker = 'broker.emqx.io'
+port = 1883
+topic = 'petCare'
+client_id = f'python-mqtt-1'
+username = ''
+password = ''
 
 
-def create_mqtt_app():
+def get_mqtt_client():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
 
-    # Setup connection to mqtt broker
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.config['MQTT_BROKER_URL'] = 'localhost'
-    app.config['MQTT_BROKER_PORT'] = 1883
-    app.config['MQTT_USERNAME'] = ''
-    app.config['MQTT_PASSWORD'] = ''
-    app.config['MQTT_KEEPALIVE'] = 5
-    app.config['MQTT_TLS_ENABLED'] = False
-    app.config['MQTT_CLEAN_SESSION'] = True
-
-    global mqtt
-    mqtt = Mqtt(app)
-    global socketio
-    socketio = SocketIO(app, async_mode="eventlet")
-
-    return mqtt
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
 
-# Function that every second publishes a message
-def background_thread():
-    count = 0
-    while True:
-        time.sleep(1)
-        # Using app context is required because the get_status() functions
-        # requires access to the db.
-        with app.app_context():
-            message = json.dumps(status.get_status(), default=str)
-        # Publish
-        mqtt.publish('python/mqtt', message)
+def publish_message(client, message):
+    result = client.publish(topic, message)
+    status = result[0]
+
+    if status == 0:
+        print(f'Sent message `{message}`')
+        return 'ok'
+    else:
+        print(f'Failed to send message to topic {topic}')
+        return 'fail'
 
 
 def create_app():
@@ -72,17 +74,15 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
-    # app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+    client = get_mqtt_client()
+
+    app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
     # ### end swagger specific ###
-    # app.register_blueprint(water.get_blueprint())
+    app.register_blueprint(water.get_blueprint())
 
     @app.route('/')
     def hello_world():
-        global thread
-        if thread is None:
-            thread = Thread(target=background_thread)
-            thread.daemon = True
-            thread.start()
+        publish_message(client, 'Hello world from mqtt')
         return 'Hello World!'
 
     db.init_app(app)
@@ -96,35 +96,13 @@ def create_app():
     return app
 
 
-def mqttClient():
-    mqttClient = mqtt.Client()
-    mqttClient.username_pw_set('', '')
-    topics = ['food/level', 'water/level', 'thermometer/level']
-
-    def on_message(client, userdata, msg):
-        print('###################')
-        print(msg)
-
-    for topic in topics:
-        mqttClient.subscribe(topic)
-        mqtt.on_message = on_message
-
-    mqttClient.connect('localhost')
-    mqttClient.loop_start()
-
-
 def run_socketio_app():
-    global socketio
     create_app()
     socketio = SocketIO(app)
-    bootstrap = Bootstrap(app)
-    create_mqtt_app()
-    socketio.run(app, host='localhost', port=5000,
-                 use_reloader=False, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000,
+                 use_reloader=True, debug=True)
 
 
 if __name__ == '__main__':
     os.environ['FLASK_ENV'] = 'development'
-    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=True, debug=True)
-    mqttClient()
     run_socketio_app()
--- BE-Flask/water.py
+++ BE-Flask/water.py
@@ -1,17 +1,20 @@
+import ButtonForFeedingModel
 from flask import (
     Blueprint, request, jsonify
 )
-import os,sys
+import os
+import sys
 from db import get_db
 from db_v2 import DB
+from app import publish_message, get_mqtt_client
 
 bp = Blueprint('water', __name__, url_prefix='/water')
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'ButtonForFeeding'))
-import ButtonForFeedingModel
 
 db2 = DB()
-waterButton =  ButtonForFeedingModel.ButtonForFeeding(10,5,1,'Water')
+waterButton = ButtonForFeedingModel.ButtonForFeeding(10, 5, 1, 'Water')
+
 
 def get_blueprint():
     """Return the blueprint for the main app module"""
@@ -20,30 +23,48 @@ def get_blueprint():
 
 @bp.route('/', methods=('GET', 'POST'))
 def get_water():
+    client = get_mqtt_client()
+    publish_message(client, 'Get water history')
     return jsonify({
         'table': 'Water',
-        'rows':db2.getTableData('water')}), 200
+        'rows': db2.getTableData('water')}), 200
 
-@bp.route('/start-water-sensor',methods=('GET', 'POST'))
+
+@bp.route('/start-water-sensor', methods=('GET', 'POST'))
 def startSensor():
     waterButton.startSensor()
-    return 'Water sensor is opened',200
+    client = get_mqtt_client()
+    publish_message(client, 'Start water sensor')
+    return 'Water sensor is opened', 200
 
-@bp.route('/stop-water-sensor',methods=('GET', 'POST'))
+
+@bp.route('/stop-water-sensor', methods=('GET', 'POST'))
 def stopSensor():
     waterButton.stopSensor()
-    return 'Water sensor is closed',200
+    client = get_mqtt_client()
+    publish_message(client, 'Stop water sensor')
+    return 'Water sensor is closed', 200
 
-@bp.route('/get-water-level',methods=('GET', 'POST'))
+
+@bp.route('/get-water-level', methods=('GET', 'POST'))
 def getFeedingLevel():
-    return f'Your water level is {waterButton.getFeedingLevel()}.',200
+    client = get_mqtt_client()
+    message = 'Get water level: '+str(waterButton.getFeedingLevel())
+    publish_message(client, message)
+    return f'Your water level is {waterButton.getFeedingLevel()}.', 200
 
-@bp.route('/make-water-empty',methods=('GET', 'POST'))
+
+@bp.route('/make-water-empty', methods=('GET', 'POST'))
 def makeFeedingEmpty():
+    client = get_mqtt_client()
+    publish_message(client, 'Make current water empty')
     waterButton.makeFeedingEmpty()
-    return 'make water empty',200
+    return 'make water empty', 200
 
-@bp.route('/push-water-manual',methods=('GET', 'POST'))
+
+@bp.route('/push-water-manual', methods=('GET', 'POST'))
 def pushManuel():
+    client = get_mqtt_client()
+    publish_message(client, 'Add water manually by incremental of 10')
     waterButton.pushManual()
-    return 'Water was pushed',200
+    return 'Water was pushed', 200
